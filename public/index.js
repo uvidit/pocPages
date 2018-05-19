e9ea685d11bda6eb88f6a4f4399df4e330ae386c
uvidit <uvidit@gmail.com>
2018-05-19 11:47:48 +0300

    Updated report data

--- public/index.js
+++ public/index.js
@@ -2,107 +2,63 @@
 
 allure.api.addTranslation('en', {
     tab: {
-        behaviors: {
-            name: 'Behaviors'
-        }
-    },
-    widget: {
-        behaviors: {
-            name: 'Features by stories',
-            showAll: 'show all'
+        packages: {
+            name: 'Packages'
         }
     }
 });
 
 allure.api.addTranslation('ru', {
     tab: {
-        behaviors: {
-            name: 'Функциональность'
-        }
-    },
-    widget: {
-        behaviors: {
-            name: 'Функциональность',
-            showAll: 'показать все'
+        packages: {
+            name: 'Пакеты'
         }
     }
 });
 
 allure.api.addTranslation('zh', {
     tab: {
-        behaviors: {
-            name: '功能'
-        }
-    },
-    widget: {
-        behaviors: {
-            name: '特性场景',
-            showAll: '显示所有'
+        packages: {
+            name: '包'
         }
     }
 });
 
 allure.api.addTranslation('de', {
     tab: {
-        behaviors: {
-            name: 'Verhalten'
-        }
-    },
-    widget: {
-        behaviors: {
-            name: 'Features nach Stories',
-            showAll: 'Zeige alle'
+        packages: {
+            name: 'Pakete'
         }
     }
 });
 
 allure.api.addTranslation('he', {
     tab: {
-        behaviors: {
-            name: 'התנהגויות'
-        }
-    },
-    widget: {
-        behaviors: {
-            name: 'תכונות לפי סיפורי משתמש',
-            showAll: 'הצג הכול'
+        packages: {
+            name: 'חבילות'
         }
     }
 });
 
 allure.api.addTranslation('br', {
     tab: {
-        behaviors: {
-            name: 'Comportamentos'
-        }
-    }, 
-    widget: {
-        behaviors: {
-            name: 'Funcionalidades por história', 
-            showAll: 'Mostrar tudo'
+        packages: {
+            name: 'Pacotes'
         }
     }
 });
 
-allure.api.addTab('behaviors', {
-    title: 'tab.behaviors.name', icon: 'fa fa-list',
-    route: 'behaviors(/)(:testGroup)(/)(:testResult)(/)(:testResultTab)(/)',
+allure.api.addTab('packages', {
+    title: 'tab.packages.name', icon: 'fa fa-align-left',
+    route: 'packages(/)(:testGroup)(/)(:testResult)(/)(:testResultTab)(/)',
     onEnter: (function (testGroup, testResult, testResultTab) {
         return new allure.components.TreeLayout({
             testGroup: testGroup,
             testResult: testResult,
             testResultTab: testResultTab,
-            tabName: 'tab.behaviors.name',
-            baseUrl: 'behaviors',
-            url: 'data/behaviors.json',
-            csvUrl: 'data/behaviors.csv'
+            tabName: 'tab.packages.name',
+            baseUrl: 'packages',
+            url: 'data/packages.json'
         });
     })
 });
-
-allure.api.addWidget('widgets', 'behaviors', allure.components.WidgetStatusView.extend({
-    rowTag: 'a',
-    title: 'widget.behaviors.name',
-    baseUrl: 'behaviors',
-    showLinks: true
-}));
