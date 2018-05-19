bf0a84ffe55315fe2f2e9c2c5906fbd9cee07f94
uvidit <uvidit@gmail.com>
2018-05-19 16:56:40 +0300

    some cleaning up and logging

--- publishing.sh
+++ publishing.sh
@@ -5,14 +5,8 @@ BUCKET_NAME=allure-poc-prjct-bucket
 #clean up public dir
 rm -rf ./public/**
 
-# installing Google SDK
-#GCLOUD_DIR=google-cloud-sdk
 echo "Google SDK install...."
-#curl https://sdk.cloud.google.com | exec bash #| echo "downloading in progress..."
-#chmod +x ~/${GCLOUD_DIR}/install.sh
-#CLOUDSDK_CORE_DISABLE_PROMPTS=1 ~/${GCLOUD_DIR}/install.sh
 
-#$HOME/google-cloud-sdk/
 gcloud version || true
 if [ ! -d "$HOME/google-cloud-sdk/bin" ]; then rm -rf $HOME/google-cloud-sdk; export CLOUDSDK_CORE_DISABLE_PROMPTS=1; curl https://sdk.cloud.google.com | bash; fi
 # Add gcloud to $PATH
@@ -26,6 +20,7 @@ gcloud auth activate-service-account \
     gc-svc-acc-allure-reporter-prj@test-gce-prjct.iam.gserviceaccount.com \
     --key-file ./test-gce-prjct-dbc00919a7ea.json
 gcloud config set project test-gce-prjct
+
 echo "Google SDK info:"
 gcloud info
 
@@ -33,8 +28,10 @@ echo "Google SDK downloaded."
 echo "Creating Bucket on Google Storage....."
 
 gsutil -m cp gs://${BUCKET_NAME}/allure-report/** ./public/
+echo "Check file list in 'public':"
 ls ./public
 
+echo "Adding/comitting/pushing...."
 git add ./public/*
 git commit -m "Updated report data"
 git push origin HEAD:master
